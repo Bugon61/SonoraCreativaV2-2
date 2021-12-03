05d9fb6d5e75cee84e01ac09c8ec8449920e812d
alejandra duarte <ale_duarte13@hotmail.com>
2021-12-03 05:12:14 -0700

    colocacion del logo principal y label de menu principal

--- SonoraCreativaV2-2/Menu Principal Employee.Designer.cs
+++ SonoraCreativaV2-2/Menu Principal Employee.Designer.cs
@@ -32,6 +32,9 @@
             this.buttonPayment = new System.Windows.Forms.Button();
             this.buttonInventory = new System.Windows.Forms.Button();
             this.buttonLogout = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonPayment
@@ -39,7 +42,7 @@
             this.buttonPayment.BackColor = System.Drawing.Color.Peru;
             this.buttonPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonPayment.Location = new System.Drawing.Point(483, 219);
-            this.buttonPayment.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonPayment.Margin = new System.Windows.Forms.Padding(2);
             this.buttonPayment.Name = "buttonPayment";
             this.buttonPayment.Size = new System.Drawing.Size(292, 169);
             this.buttonPayment.TabIndex = 32;
@@ -51,7 +54,7 @@
             this.buttonInventory.BackColor = System.Drawing.Color.Peru;
             this.buttonInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonInventory.Location = new System.Drawing.Point(178, 219);
-            this.buttonInventory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonInventory.Margin = new System.Windows.Forms.Padding(2);
             this.buttonInventory.Name = "buttonInventory";
             this.buttonInventory.Size = new System.Drawing.Size(292, 169);
             this.buttonInventory.TabIndex = 31;
@@ -62,12 +65,34 @@
             // 
             this.buttonLogout.BackColor = System.Drawing.Color.Peru;
             this.buttonLogout.Location = new System.Drawing.Point(854, 10);
-            this.buttonLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(2);
             this.buttonLogout.Name = "buttonLogout";
             this.buttonLogout.Size = new System.Drawing.Size(106, 30);
             this.buttonLogout.TabIndex = 30;
             this.buttonLogout.Text = "Cerrar Sesión";
             this.buttonLogout.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Image = global::SonoraCreativaV2_2.Properties.Resources.Logo_Sonora_Creativa___copia;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(262, 104);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 34;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(331, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 39);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Menú Principal";
             // 
             // Menu_Principal_Employee
             // 
@@ -75,14 +100,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PapayaWhip;
             this.ClientSize = new System.Drawing.Size(968, 605);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.buttonPayment);
             this.Controls.Add(this.buttonInventory);
             this.Controls.Add(this.buttonLogout);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Menu_Principal_Employee";
             this.Text = "Menu Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,5 +120,7 @@
         private System.Windows.Forms.Button buttonPayment;
         private System.Windows.Forms.Button buttonInventory;
         private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
