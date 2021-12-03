a066b3ea9b56edf9c5d097df66be2dd49825a367
alejandra duarte <ale_duarte13@hotmail.com>
2021-12-03 04:03:42 -0700

    colores en fondo y boton agrege icono en ventana

--- SonoraCreativaV2-2/Users/Cambiar contrasena.Designer.cs
+++ SonoraCreativaV2-2/Users/Cambiar contrasena.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cambiar_contrasena));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textBoxAdminUsername = new System.Windows.Forms.TextBox();
             this.textBoxAdminPW = new System.Windows.Forms.TextBox();
@@ -53,42 +54,48 @@
             this.groupBox1.Controls.Add(this.textBoxAdminPW);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(375, 20);
+            this.groupBox1.Location = new System.Drawing.Point(281, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(358, 214);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(268, 174);
             this.groupBox1.TabIndex = 29;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Introducir datos de administrador";
             // 
             // textBoxAdminUsername
             // 
-            this.textBoxAdminUsername.Location = new System.Drawing.Point(154, 29);
+            this.textBoxAdminUsername.Location = new System.Drawing.Point(116, 24);
+            this.textBoxAdminUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxAdminUsername.Name = "textBoxAdminUsername";
-            this.textBoxAdminUsername.Size = new System.Drawing.Size(198, 22);
+            this.textBoxAdminUsername.Size = new System.Drawing.Size(150, 20);
             this.textBoxAdminUsername.TabIndex = 7;
             // 
             // textBoxAdminPW
             // 
-            this.textBoxAdminPW.Location = new System.Drawing.Point(154, 77);
+            this.textBoxAdminPW.Location = new System.Drawing.Point(116, 63);
+            this.textBoxAdminPW.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxAdminPW.Name = "textBoxAdminPW";
-            this.textBoxAdminPW.Size = new System.Drawing.Size(198, 22);
+            this.textBoxAdminPW.Size = new System.Drawing.Size(150, 20);
             this.textBoxAdminPW.TabIndex = 25;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 32);
+            this.label4.Location = new System.Drawing.Point(17, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(125, 16);
+            this.label4.Size = new System.Drawing.Size(99, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Nombre de usuario:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(72, 80);
+            this.label3.Location = new System.Drawing.Point(54, 65);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.Size = new System.Drawing.Size(64, 13);
             this.label3.TabIndex = 24;
             this.label3.Text = "Contraseña:";
             // 
@@ -102,42 +109,48 @@
             this.groupBoxEUser.Controls.Add(this.label10);
             this.groupBoxEUser.Controls.Add(this.textBoxUsername);
             this.groupBoxEUser.Controls.Add(this.label1);
-            this.groupBoxEUser.Location = new System.Drawing.Point(11, 20);
+            this.groupBoxEUser.Location = new System.Drawing.Point(8, 16);
+            this.groupBoxEUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBoxEUser.Name = "groupBoxEUser";
-            this.groupBoxEUser.Size = new System.Drawing.Size(358, 214);
+            this.groupBoxEUser.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBoxEUser.Size = new System.Drawing.Size(268, 174);
             this.groupBoxEUser.TabIndex = 27;
             this.groupBoxEUser.TabStop = false;
             this.groupBoxEUser.Text = "Usuario a editar";
             // 
             // textBoxCPassword
             // 
-            this.textBoxCPassword.Location = new System.Drawing.Point(146, 176);
+            this.textBoxCPassword.Location = new System.Drawing.Point(110, 143);
+            this.textBoxCPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxCPassword.Name = "textBoxCPassword";
-            this.textBoxCPassword.Size = new System.Drawing.Size(198, 22);
+            this.textBoxCPassword.Size = new System.Drawing.Size(150, 20);
             this.textBoxCPassword.TabIndex = 23;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 179);
+            this.label7.Location = new System.Drawing.Point(4, 145);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(137, 16);
+            this.label7.Size = new System.Drawing.Size(110, 13);
             this.label7.TabIndex = 22;
             this.label7.Text = "Confirmar contraseña:";
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(146, 126);
+            this.textBoxPassword.Location = new System.Drawing.Point(110, 102);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(198, 22);
+            this.textBoxPassword.Size = new System.Drawing.Size(150, 20);
             this.textBoxPassword.TabIndex = 21;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(64, 129);
+            this.label6.Location = new System.Drawing.Point(48, 105);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(79, 16);
+            this.label6.Size = new System.Drawing.Size(64, 13);
             this.label6.TabIndex = 20;
             this.label6.Text = "Contraseña:";
             // 
@@ -149,56 +162,65 @@
             "Matutino Parcial",
             "Vespertino (14:00-22:00)",
             "Vespertino Parcial"});
-            this.comboBoxSelectId.Location = new System.Drawing.Point(146, 32);
+            this.comboBoxSelectId.Location = new System.Drawing.Point(110, 26);
+            this.comboBoxSelectId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBoxSelectId.Name = "comboBoxSelectId";
-            this.comboBoxSelectId.Size = new System.Drawing.Size(198, 24);
+            this.comboBoxSelectId.Size = new System.Drawing.Size(150, 21);
             this.comboBoxSelectId.TabIndex = 19;
             this.comboBoxSelectId.Text = "Seleccione id...";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(8, 35);
+            this.label10.Location = new System.Drawing.Point(6, 28);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(132, 16);
+            this.label10.Size = new System.Drawing.Size(105, 13);
             this.label10.TabIndex = 18;
             this.label10.Text = "Seleccionar Usuario:";
             // 
             // textBoxUsername
             // 
-            this.textBoxUsername.Location = new System.Drawing.Point(146, 80);
+            this.textBoxUsername.Location = new System.Drawing.Point(110, 65);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(198, 22);
+            this.textBoxUsername.Size = new System.Drawing.Size(150, 20);
             this.textBoxUsername.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 83);
+            this.label1.Location = new System.Drawing.Point(11, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(125, 16);
+            this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre de usuario:";
             // 
             // buttonConfirm
             // 
-            this.buttonConfirm.Location = new System.Drawing.Point(300, 240);
+            this.buttonConfirm.BackColor = System.Drawing.Color.Goldenrod;
+            this.buttonConfirm.Location = new System.Drawing.Point(225, 195);
+            this.buttonConfirm.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonConfirm.Name = "buttonConfirm";
-            this.buttonConfirm.Size = new System.Drawing.Size(141, 37);
+            this.buttonConfirm.Size = new System.Drawing.Size(106, 30);
             this.buttonConfirm.TabIndex = 28;
             this.buttonConfirm.Text = "Confirmar";
-            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.UseVisualStyleBackColor = false;
             // 
             // Cambiar_contrasena
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(745, 296);
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(559, 240);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBoxEUser);
             this.Controls.Add(this.buttonConfirm);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Cambiar_contrasena";
-            this.Text = "Cambiar_contrasena";
+            this.Text = "Cambiar Contrasena";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBoxEUser.ResumeLayout(false);
