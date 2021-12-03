effb3500b22ac6074871c05b9fd47c3f5e62c1c9
alejandra duarte <ale_duarte13@hotmail.com>
2021-12-03 04:10:53 -0700

    agrege icono en ventana cambio de colores en dataGridView1 colores en fondo y botones

--- SonoraCreativaV2-2/Inventory/InventarioEmpleado.Designer.cs
+++ SonoraCreativaV2-2/Inventory/InventarioEmpleado.Designer.cs
@@ -29,53 +29,60 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InventarioEmpleado));
             this.buttonPMenu = new System.Windows.Forms.Button();
             this.buttonLogout = new System.Windows.Forms.Button();
             this.groupBoxGridView = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.buttonInfoProduct = new System.Windows.Forms.Button();
-            this.buttonSearchProduct = new System.Windows.Forms.Button();
-            this.textBoxSearchProduct = new System.Windows.Forms.TextBox();
-            this.inventarioEmpleadosDS = new SonoraCreativaV2_2.InventarioEmpleadosDS();
-            this.inventarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.inventarioTableAdapter = new SonoraCreativaV2_2.InventarioEmpleadosDSTableAdapters.inventarioTableAdapter();
             this.idproductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tipoproductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cantidadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.precioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.inventarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.inventarioEmpleadosDS = new SonoraCreativaV2_2.InventarioEmpleadosDS();
+            this.buttonInfoProduct = new System.Windows.Forms.Button();
+            this.buttonSearchProduct = new System.Windows.Forms.Button();
+            this.textBoxSearchProduct = new System.Windows.Forms.TextBox();
+            this.inventarioTableAdapter = new SonoraCreativaV2_2.InventarioEmpleadosDSTableAdapters.inventarioTableAdapter();
             this.groupBoxGridView.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inventarioEmpleadosDS)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inventarioBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventarioEmpleadosDS)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonPMenu
             // 
+            this.buttonPMenu.BackColor = System.Drawing.Color.Peru;
             this.buttonPMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonPMenu.Location = new System.Drawing.Point(18, 12);
+            this.buttonPMenu.Location = new System.Drawing.Point(14, 10);
+            this.buttonPMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonPMenu.Name = "buttonPMenu";
-            this.buttonPMenu.Size = new System.Drawing.Size(141, 37);
+            this.buttonPMenu.Size = new System.Drawing.Size(106, 30);
             this.buttonPMenu.TabIndex = 25;
             this.buttonPMenu.Text = "Menu principal";
-            this.buttonPMenu.UseVisualStyleBackColor = true;
+            this.buttonPMenu.UseVisualStyleBackColor = false;
             this.buttonPMenu.Click += new System.EventHandler(this.buttonPMenu_Click);
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(1138, 12);
+            this.buttonLogout.BackColor = System.Drawing.Color.Peru;
+            this.buttonLogout.Location = new System.Drawing.Point(854, 10);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(141, 37);
+            this.buttonLogout.Size = new System.Drawing.Size(106, 30);
             this.buttonLogout.TabIndex = 24;
             this.buttonLogout.Text = "Cerrar Sesión";
-            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.UseVisualStyleBackColor = false;
             // 
             // groupBoxGridView
             // 
             this.groupBoxGridView.Controls.Add(this.dataGridView1);
-            this.groupBoxGridView.Location = new System.Drawing.Point(12, 125);
+            this.groupBoxGridView.Location = new System.Drawing.Point(9, 102);
+            this.groupBoxGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBoxGridView.Name = "groupBoxGridView";
-            this.groupBoxGridView.Size = new System.Drawing.Size(1267, 608);
+            this.groupBoxGridView.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBoxGridView.Size = new System.Drawing.Size(950, 494);
             this.groupBoxGridView.TabIndex = 23;
             this.groupBoxGridView.TabStop = false;
             this.groupBoxGridView.Text = "Seleccione el producto que desea revisar";
@@ -83,6 +90,7 @@
             // dataGridView1
             // 
             this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.idproductoDataGridViewTextBoxColumn,
@@ -91,52 +99,13 @@
             this.cantidadDataGridViewTextBoxColumn,
             this.precioDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.inventarioBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 21);
+            this.dataGridView1.Location = new System.Drawing.Point(4, 17);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1255, 581);
+            this.dataGridView1.Size = new System.Drawing.Size(941, 472);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // buttonInfoProduct
-            // 
-            this.buttonInfoProduct.Location = new System.Drawing.Point(462, 82);
-            this.buttonInfoProduct.Name = "buttonInfoProduct";
-            this.buttonInfoProduct.Size = new System.Drawing.Size(141, 37);
-            this.buttonInfoProduct.TabIndex = 21;
-            this.buttonInfoProduct.Text = "Ver Informacion";
-            this.buttonInfoProduct.UseVisualStyleBackColor = true;
-            // 
-            // buttonSearchProduct
-            // 
-            this.buttonSearchProduct.Location = new System.Drawing.Point(315, 82);
-            this.buttonSearchProduct.Name = "buttonSearchProduct";
-            this.buttonSearchProduct.Size = new System.Drawing.Size(141, 37);
-            this.buttonSearchProduct.TabIndex = 20;
-            this.buttonSearchProduct.Text = "Buscar";
-            this.buttonSearchProduct.UseVisualStyleBackColor = true;
-            // 
-            // textBoxSearchProduct
-            // 
-            this.textBoxSearchProduct.Location = new System.Drawing.Point(18, 89);
-            this.textBoxSearchProduct.Name = "textBoxSearchProduct";
-            this.textBoxSearchProduct.Size = new System.Drawing.Size(291, 22);
-            this.textBoxSearchProduct.TabIndex = 26;
-            this.textBoxSearchProduct.Text = "Buscar producto...";
-            // 
-            // inventarioEmpleadosDS
-            // 
-            this.inventarioEmpleadosDS.DataSetName = "InventarioEmpleadosDS";
-            this.inventarioEmpleadosDS.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // inventarioBindingSource
-            // 
-            this.inventarioBindingSource.DataMember = "inventario";
-            this.inventarioBindingSource.DataSource = this.inventarioEmpleadosDS;
-            // 
-            // inventarioTableAdapter
-            // 
-            this.inventarioTableAdapter.ClearBeforeFill = true;
             // 
             // idproductoDataGridViewTextBoxColumn
             // 
@@ -179,24 +148,72 @@
             this.precioDataGridViewTextBoxColumn.Name = "precioDataGridViewTextBoxColumn";
             this.precioDataGridViewTextBoxColumn.Width = 125;
             // 
+            // inventarioBindingSource
+            // 
+            this.inventarioBindingSource.DataMember = "inventario";
+            this.inventarioBindingSource.DataSource = this.inventarioEmpleadosDS;
+            // 
+            // inventarioEmpleadosDS
+            // 
+            this.inventarioEmpleadosDS.DataSetName = "InventarioEmpleadosDS";
+            this.inventarioEmpleadosDS.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // buttonInfoProduct
+            // 
+            this.buttonInfoProduct.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.buttonInfoProduct.Location = new System.Drawing.Point(346, 67);
+            this.buttonInfoProduct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonInfoProduct.Name = "buttonInfoProduct";
+            this.buttonInfoProduct.Size = new System.Drawing.Size(106, 30);
+            this.buttonInfoProduct.TabIndex = 21;
+            this.buttonInfoProduct.Text = "Ver Informacion";
+            this.buttonInfoProduct.UseVisualStyleBackColor = false;
+            // 
+            // buttonSearchProduct
+            // 
+            this.buttonSearchProduct.BackColor = System.Drawing.Color.Gold;
+            this.buttonSearchProduct.Location = new System.Drawing.Point(236, 67);
+            this.buttonSearchProduct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSearchProduct.Name = "buttonSearchProduct";
+            this.buttonSearchProduct.Size = new System.Drawing.Size(106, 30);
+            this.buttonSearchProduct.TabIndex = 20;
+            this.buttonSearchProduct.Text = "Buscar";
+            this.buttonSearchProduct.UseVisualStyleBackColor = false;
+            // 
+            // textBoxSearchProduct
+            // 
+            this.textBoxSearchProduct.Location = new System.Drawing.Point(14, 72);
+            this.textBoxSearchProduct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxSearchProduct.Name = "textBoxSearchProduct";
+            this.textBoxSearchProduct.Size = new System.Drawing.Size(219, 20);
+            this.textBoxSearchProduct.TabIndex = 26;
+            this.textBoxSearchProduct.Text = "Buscar producto...";
+            // 
+            // inventarioTableAdapter
+            // 
+            this.inventarioTableAdapter.ClearBeforeFill = true;
+            // 
             // InventarioEmpleado
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1291, 745);
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(968, 605);
             this.Controls.Add(this.textBoxSearchProduct);
             this.Controls.Add(this.buttonPMenu);
             this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.groupBoxGridView);
             this.Controls.Add(this.buttonInfoProduct);
             this.Controls.Add(this.buttonSearchProduct);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "InventarioEmpleado";
             this.Text = "Inventario";
             this.Load += new System.EventHandler(this.InventarioEmpleado_Load);
             this.groupBoxGridView.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inventarioEmpleadosDS)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.inventarioBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventarioEmpleadosDS)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
