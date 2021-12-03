9267fc4ac7a3a3630c3898a6598515a3095acf33
alejandra duarte <ale_duarte13@hotmail.com>
2021-12-03 03:43:51 -0700

    logo en la ventana y colores

--- SonoraCreativaV2-2/Menu principal.Designer.cs
+++ SonoraCreativaV2-2/Menu principal.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu_principal));
             this.buttonLogout = new System.Windows.Forms.Button();
             this.buttonInventory = new System.Windows.Forms.Button();
             this.buttonPayment = new System.Windows.Forms.Button();
@@ -37,68 +38,81 @@
             // 
             // buttonLogout
             // 
-            this.buttonLogout.Location = new System.Drawing.Point(1138, 12);
+            this.buttonLogout.BackColor = System.Drawing.Color.Peru;
+            this.buttonLogout.Location = new System.Drawing.Point(854, 10);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(141, 37);
+            this.buttonLogout.Size = new System.Drawing.Size(106, 30);
             this.buttonLogout.TabIndex = 25;
             this.buttonLogout.Text = "Cerrar Sesión";
-            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.UseVisualStyleBackColor = false;
             this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
             // 
             // buttonInventory
             // 
+            this.buttonInventory.BackColor = System.Drawing.Color.Peru;
             this.buttonInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonInventory.Location = new System.Drawing.Point(245, 167);
+            this.buttonInventory.Location = new System.Drawing.Point(184, 136);
+            this.buttonInventory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonInventory.Name = "buttonInventory";
-            this.buttonInventory.Size = new System.Drawing.Size(389, 208);
+            this.buttonInventory.Size = new System.Drawing.Size(292, 169);
             this.buttonInventory.TabIndex = 26;
             this.buttonInventory.Text = "Inventario";
-            this.buttonInventory.UseVisualStyleBackColor = true;
+            this.buttonInventory.UseVisualStyleBackColor = false;
             this.buttonInventory.Click += new System.EventHandler(this.button1_Click);
             // 
             // buttonPayment
             // 
+            this.buttonPayment.BackColor = System.Drawing.Color.Peru;
             this.buttonPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonPayment.Location = new System.Drawing.Point(651, 167);
+            this.buttonPayment.Location = new System.Drawing.Point(488, 136);
+            this.buttonPayment.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonPayment.Name = "buttonPayment";
-            this.buttonPayment.Size = new System.Drawing.Size(389, 208);
+            this.buttonPayment.Size = new System.Drawing.Size(292, 169);
             this.buttonPayment.TabIndex = 27;
             this.buttonPayment.Text = "Cobros";
-            this.buttonPayment.UseVisualStyleBackColor = true;
+            this.buttonPayment.UseVisualStyleBackColor = false;
             this.buttonPayment.Click += new System.EventHandler(this.buttonPayment_Click);
             // 
             // buttonSales
             // 
+            this.buttonSales.BackColor = System.Drawing.Color.Peru;
             this.buttonSales.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSales.Location = new System.Drawing.Point(651, 392);
+            this.buttonSales.Location = new System.Drawing.Point(488, 318);
+            this.buttonSales.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSales.Name = "buttonSales";
-            this.buttonSales.Size = new System.Drawing.Size(389, 208);
+            this.buttonSales.Size = new System.Drawing.Size(292, 169);
             this.buttonSales.TabIndex = 29;
             this.buttonSales.Text = "Ventas";
-            this.buttonSales.UseVisualStyleBackColor = true;
+            this.buttonSales.UseVisualStyleBackColor = false;
             this.buttonSales.Click += new System.EventHandler(this.buttonSales_Click);
             // 
             // buttonUsers
             // 
+            this.buttonUsers.BackColor = System.Drawing.Color.Peru;
             this.buttonUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonUsers.Location = new System.Drawing.Point(245, 392);
+            this.buttonUsers.Location = new System.Drawing.Point(184, 318);
+            this.buttonUsers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonUsers.Name = "buttonUsers";
-            this.buttonUsers.Size = new System.Drawing.Size(389, 208);
+            this.buttonUsers.Size = new System.Drawing.Size(292, 169);
             this.buttonUsers.TabIndex = 28;
             this.buttonUsers.Text = "Usuarios";
-            this.buttonUsers.UseVisualStyleBackColor = true;
+            this.buttonUsers.UseVisualStyleBackColor = false;
             this.buttonUsers.Click += new System.EventHandler(this.buttonUsers_Click);
             // 
             // Menu_principal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1291, 745);
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(968, 605);
             this.Controls.Add(this.buttonSales);
             this.Controls.Add(this.buttonUsers);
             this.Controls.Add(this.buttonPayment);
             this.Controls.Add(this.buttonInventory);
             this.Controls.Add(this.buttonLogout);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Menu_principal";
             this.Text = "Menu principal";
             this.ResumeLayout(false);
